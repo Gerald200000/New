5efe04f615ad567d2d38369a5dbc09894b8c0388
Gerald200000 <gerald30.munyoro@gmail.com>
2025-06-08 20:18:20 +0200

    Second Commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
-from .views import post_list, post_detail
+from .views import BlogDetailView, BlogListView, BlogCreateView
 from django.urls import path
 
-urlpatterns = [path("",post_list,name = 'home'),
-                path("post/<int:pk>/", post_detail, name = "post_detail")
+urlpatterns = [path("",BlogListView.as_view(),name = 'home'),
+                path("post/<int:pk>/", BlogDetailView.as_view(), name = "post_detail"),
+                path("post/new/", BlogCreateView.as_view(),name = 'post_new')
                ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
-# blog/views.py
-from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
 
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'home.html', {'posts': posts})
-
-def post_detail(request,pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'post_detail.html', {'post': post})
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title","author","body"]
+    
