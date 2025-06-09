5346e97747ec541884964890855561b1c9fff58f
Gerald200000 <gerald30.munyoro@gmail.com>
2025-06-09 13:55:59 +0200

    Fifth Commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
-from .views import BlogDetailView, BlogListView, BlogCreateView, BlogUpdateView
+from .views import BlogDetailView, BlogListView, BlogCreateView, BlogUpdateView, BlogDeleteView
 from django.urls import path
 
 urlpatterns = [path("",BlogListView.as_view(),name = 'home'),
                 path("post/<int:pk>/", BlogDetailView.as_view(), name = "post_detail"),
                 path("post/new/", BlogCreateView.as_view(),name = 'post_new'),
-                path("post/<int:pk>/edit", BlogUpdateView.as_view(),name = "post_edit")
+                path("post/<int:pk>/edit/", BlogUpdateView.as_view(),name = "post_edit"),
+                path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name = "post_delete")
                ]
